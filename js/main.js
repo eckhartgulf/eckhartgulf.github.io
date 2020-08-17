53f0179ec9aa6dfa92f026339fb75b78ee6ffdb4
Sinwan <sinwan@kefassoftwares.com>
2020-08-18 01:45:10 +0530

    small changes

--- js/main.js
+++ js/main.js
@@ -277,4 +277,16 @@ jQuery(document).ready(function($) {
 	};
 	searchShow();
 
+	$('.nav-link').on('click',function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	$('.nav-item').on('click',function(){
+
+		//Remove any previous active classes
+		$('.nav-item').removeClass('active');
+	
+		//Add active class to the clicked item
+		$(this).addClass('active');
+	});
 });
